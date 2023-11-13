c44ad743c74f0ae95e81ba9d0cfa6d72f092dc9b
Balazs <balzs.gyngysi@gmail.com>
2023-11-13 19:36:21 +0100

    added tmp heaven and hell buttons, scale calculation added with tmp labels

---
+++ Label.gd
@@ -0,0 +1,17 @@
+extends Label
+
+class_name Scale
+@onready var scale_label = %ScaleValue
+
+var score = 0
+
+func _ready():
+	_update_score_label(score)
+
+func _update_score_label(score):
+	if (score < 0):
+		scale_label.text = "Hell felé billenek :" + str(score)
+	elif (score > 0):
+		scale_label.text ="Heavel felé billenek :" + str(score)
+	else:
+		scale_label.text = "Balance"
---
+++ scale.gd
@@ -0,0 +1,34 @@
+extends Control
+
+@onready var scale_vale_node = $ScaleValue
+@onready var tmp_next_soul = $TmpNextSoul
+
+var tmp_character_soul_value = tmp_get_random_soul_value()
+var heaven_score = -50
+var hell_score = 50
+
+func _on_hell_button_pressed():
+	print("hell button pressed")
+	hell_score = hell_score + tmp_character_soul_value
+	scale_vale_node._update_score_label(hell_score+heaven_score)
+	tmp_character_soul_value = tmp_get_random_soul_value()
+	tmp_set_soul_forecast()
+
+
+func _on_heaven_button_pressed():
+	print("heaven button pressed")
+	heaven_score = heaven_score + tmp_character_soul_value
+	scale_vale_node._update_score_label(hell_score+heaven_score)
+	tmp_character_soul_value = tmp_get_random_soul_value()
+	tmp_set_soul_forecast()
+
+func tmp_get_random_soul_value():
+	return randi_range(-10,10)
+	
+func tmp_set_soul_forecast():
+	if(tmp_character_soul_value < 0):
+		tmp_next_soul.text = "EVIL HUMAN: " + str(tmp_character_soul_value)
+	elif (tmp_character_soul_value > 0):
+		tmp_next_soul.text = "GOOD HUMAN: " + str(tmp_character_soul_value)	
+	else:
+		tmp_next_soul.text = "NEUTRAL HUMAN: " + str(tmp_character_soul_value)
