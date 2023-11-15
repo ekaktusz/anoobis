ede0e3b8d51e974f8137430eaf89848dc18ae162
danigeri <danigerci@gmail.com>
2023-11-15 15:01:01 +0100

    Renamed scene root nodes. Character node is deleted and recreated on ToHeaven.

--- tinder_scene.gd
+++ tinder_scene.gd
@@ -19,8 +19,11 @@ func _on_button_pressed():
 func _on_heaven_button_pressed() -> void:
 	var character_scene : PackedScene = preload("res://character.tscn")
 	var character_node : Node = character_scene.instantiate()
+	var portrait_containter : Node = $MarginContainer/HBoxContainer/MidVboxContainer/CharacterViewScene/MarginContainer
 
-	$MarginContainer/HBoxContainer/MidVboxContainer/CharacterViewScene/MarginContainer.add_child(character_node)
+#	TODO: Here we can send the portrait to the afterlife instead of deleting
+	portrait_containter.get_child(0).queue_free()
+	portrait_containter.add_child(character_node)
 
 
 func _on_hell_button_pressed() -> void:
