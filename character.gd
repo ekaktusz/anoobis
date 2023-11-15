ef8dd6ab82f22c15cd4d7161600f616360865f0e
danigeri <danigerci@gmail.com>
2023-11-15 17:57:42 +0100

    Face part asset loader added. Ugly

--- character.gd
+++ character.gd
@@ -1,74 +1,63 @@
 extends AspectRatioContainer
 
-var hair_fronts : Array = [
-	null,
-	preload ("res://images/face_parts/hair_A_front.png"),
-	preload ("res://images/face_parts/hair_B_front.png"),
-	preload ("res://images/face_parts/hair_C_front.png"),
-	preload ("res://images/face_parts/hair_D_front.png"),
-]
 
-#TODO: the size should match worked around by padding with null
-var hair_backs : Array = [
-	null,
-	preload ("res://images/face_parts/hair_A_back.png"),
-	preload ("res://images/face_parts/hair_B_back.png"),
-	null,
-	null,
-]
+class Hair:
+	var hair_back : Resource
+	var hair_front : Resource
 
-var eyes : Array = [
-	preload ("res://images/face_parts/eyes_A.png"),
-	preload ("res://images/face_parts/eyes_B.png"),
-	preload ("res://images/face_parts/eyes_C.png"),
-	preload ("res://images/face_parts/eyes_D.png"),
-]
+var eyes : Array[Resource]
+var skins : Array[Resource]
+var noses : Array[Resource]
+var mouths : Array[Resource]
+var hairs : Array[Hair]
 
-#TODO: Skin tones are mashed together with skin shapes
-var skins : Array = [
-	preload ("res://images/face_parts/skin_A1.png"),
-	preload ("res://images/face_parts/skin_A2.png"),
-	preload ("res://images/face_parts/skin_B1.png"),
-	preload ("res://images/face_parts/skin_B2.png"),
-	preload ("res://images/face_parts/skin_C1.png"),
-	preload ("res://images/face_parts/skin_C2.png"),
-	preload ("res://images/face_parts/skin_D1.png"),
-	preload ("res://images/face_parts/skin_D2.png"),
-]
-
-var noses : Array = [
-	preload ("res://images/face_parts/nose_A.png"),
-	preload ("res://images/face_parts/nose_B.png"),
-	preload ("res://images/face_parts/nose_C.png"),
-	preload ("res://images/face_parts/nose_D.png"),
-]
-
-var mouths : Array = [
-	preload ("res://images/face_parts/mouth_A.png"),
-	preload ("res://images/face_parts/mouth_B.png"),
-	preload ("res://images/face_parts/mouth_C.png"),
-	preload ("res://images/face_parts/mouth_D.png"),
-]
-
-var current_hair_front_id : int
-var current_hair_back_id : int
+var current_hair_id : int
 var current_eye_id : int
 var current_skin_id : int
 var current_nose_id : int
 var current_mouth_id : int
 
-
 func _ready() -> void:
+	var face_parts_folder = "res://images/face_parts/"
+	var dir = DirAccess.open(face_parts_folder)
+
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while (file_name != ""):
+		if file_name.ends_with("png"):
+			if file_name.begins_with("eyes"):
+				eyes.push_back(load(face_parts_folder+file_name))
+			elif file_name.begins_with("mouth"):
+				mouths.push_back(load(face_parts_folder+file_name))
+			elif file_name.begins_with("nose"):
+				noses.push_back(load(face_parts_folder+file_name))
+			elif file_name.begins_with("skin"):
+				skins.push_back(load(face_parts_folder+file_name))
+			elif file_name.begins_with("hair"):
+				if file_name.ends_with("_front.png"):
+					var hair = Hair.new()
+					hair.hair_front = load(face_parts_folder+file_name)
+					var filename_parts = file_name.split('_')
+					var back_variant = filename_parts[0] + "_" + filename_parts[1] + "_back.png"
+					print(back_variant)
+					if dir.file_exists(back_variant):
+						hair.hair_back =  load(face_parts_folder+back_variant)
+					else:
+						hair.hair_back = null
+
+					hairs.push_back(hair)
+				elif file_name.ends_with("_end.png"):
+					pass
+		file_name = dir.get_next()
+
 	randomize()
 	_set_random_parts()
 
 
 func _set_next_hair() -> void:
-	current_hair_front_id = (current_hair_front_id + 1)%hair_fronts.size()
-	$FrontHairTexture.texture = hair_fronts[current_hair_front_id]
-
-	current_hair_back_id = (current_hair_back_id + 1)%hair_backs.size()
-	$BackHairTexture.texture = hair_backs[current_hair_back_id]
+	current_hair_id = (current_hair_id + 1)%hairs.size()
+	$FrontHairTexture.texture = hairs[current_hair_id].hair_front
+	$BackHairTexture.texture = hairs[current_hair_id].hair_back
 
 
 func _set_next_eye() -> void:
@@ -92,11 +81,10 @@ func _set_next_skin() -> void:
 
 
 func _set_random_parts() -> void:
-	var rand_hair = randi()%hair_fronts.size()
-	current_hair_front_id = rand_hair
-	current_hair_back_id = rand_hair
-	$FrontHairTexture.texture = hair_fronts[current_hair_front_id]
-	$BackHairTexture.texture = hair_backs[current_hair_back_id]
+	var rand_hair = randi()%hairs.size()
+
+	$FrontHairTexture.texture = hairs[rand_hair].hair_front
+	$BackHairTexture.texture = hairs[rand_hair].hair_back
 
 	current_eye_id = randi()%eyes.size()
 	$EyesTexture.texture = eyes[current_eye_id]
