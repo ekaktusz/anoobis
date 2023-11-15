db58e1c1fc50c5db672a64b111688a186d2162ca
danigeri <danigerci@gmail.com>
2023-11-15 12:09:03 +0100

    Change buttons are working for additional face parts.

--- character_generator.gd
+++ character_generator.gd
@@ -9,9 +9,12 @@ var hair_fronts : Array = [
 	preload ("res://images/face_parts/hair_D_front.png"),
 ]
 
+#TODO: the size should match worked around by padding with null
 var hair_backs : Array = [
 	preload ("res://images/face_parts/hair_A_back.png"),
 	preload ("res://images/face_parts/hair_B_back.png"),
+	null,
+	null,
 ]
 
 var eyes : Array = [
@@ -20,6 +23,7 @@ var eyes : Array = [
 	preload ("res://images/face_parts/eyes_C.png"),
 	preload ("res://images/face_parts/eyes_D.png"),
 ]
+
 #TODO: Skin tones are mashed together with skin shapes
 var skins : Array = [
 	preload ("res://images/face_parts/skin_A1.png"),
@@ -46,17 +50,30 @@ var mouths : Array = [
 	preload ("res://images/face_parts/mouth_D.png"),
 ]
 
-var current_hair_id : int
+var current_hair_front_id : int
+var current_hair_back_id : int
 var current_eye_id : int
+var current_skin_id : int
+var current_nose_id : int
+var current_mouth_id : int
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	current_hair_id = 0
+	current_hair_front_id = 0
+	current_hair_back_id = 0
 	current_eye_id = 0
+	current_skin_id = 0
+	current_nose_id = 0
+	current_mouth_id = 0
 
-	$VBoxContainer/PortraitContainer/HairLayer/HairTexture.texture = hair_fronts[current_hair_id]
+	$VBoxContainer/PortraitContainer/FrontHairLayer/FrontHairTexture.texture = hair_fronts[current_hair_front_id]
+	$VBoxContainer/PortraitContainer/BackHairLayer/BackHairTexture.texture = hair_backs[current_hair_back_id]
 	$VBoxContainer/PortraitContainer/EyesLayer/EyesTexture.texture = eyes[current_eye_id]
+	$VBoxContainer/PortraitContainer/SkinLayer/SkinTexture.texture = skins[current_skin_id]
+	$VBoxContainer/PortraitContainer/NoseLayer/NoseTexture.texture = noses[current_nose_id]
+	$VBoxContainer/PortraitContainer/MouthLayer/MouthTexture.texture = mouths[current_mouth_id]
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -65,8 +82,11 @@ func _process(delta: float) -> void:
 
 
 func _on_hair_button_pressed() -> void:
-	current_hair_id = (current_hair_id + 1)%hair_fronts.size()
-	$VBoxContainer/PortraitContainer/HairLayer/HairTexture.texture = hair_fronts[current_hair_id]
+	current_hair_front_id = (current_hair_front_id + 1)%hair_fronts.size()
+	$VBoxContainer/PortraitContainer/FrontHairLayer/FrontHairTexture.texture = hair_fronts[current_hair_front_id]
+
+	current_hair_back_id = (current_hair_back_id + 1)%hair_backs.size()
+	$VBoxContainer/PortraitContainer/BackHairLayer/BackHairTexture.texture = hair_backs[current_hair_back_id]
 
 
 func _on_eyes_button_pressed() -> void:
@@ -79,8 +99,10 @@ func _on_ears_button_pressed() -> void:
 
 
 func _on_nose_button_pressed() -> void:
-	pass # Replace with function body.
+	current_nose_id = (current_nose_id + 1)%noses.size()
+	$VBoxContainer/PortraitContainer/NoseLayer/NoseTexture.texture = noses[current_nose_id]
 
 
 func _on_mouth_button_pressed() -> void:
-	pass # Replace with function body.
+	current_mouth_id = (current_mouth_id + 1)%mouths.size()
+	$VBoxContainer/PortraitContainer/MouthLayer/MouthTexture.texture = mouths[current_mouth_id]
