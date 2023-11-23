4365c9e1531f96c891f0e0a6632748fd6c7be0e6
danigeri <danigerci@gmail.com>
2023-11-23 13:29:16 +0100

    Heaven and hell scores are calculated and presented on underworld screens.

--- underworld.gd
+++ underworld.gd
@@ -1,8 +1,13 @@
 extends Control
 
+var hell_score : int = 0
+var heaven_score : int = 0
+
 @onready var Heaven = $Heaven
 @onready var Hell = $Hell
 @onready var UnderworldSelector = $UnderworldSelector
+@onready var HeavenScoreLabel = $Heaven/HeavenBackgroundPanel/ScoreLabel
+@onready var HellScoreLabel = $Hell/HellBackgroundPanel/ScoreLabel
 
 signal underworld_left()
 
@@ -22,3 +27,16 @@ func _on_back_pressed() -> void:
 	Hell.set_visible(false)
 
 	underworld_left.emit()
+
+
+func _on_tinder_scene_character_sent_to_heaven(character : CharacterData) -> void:
+	var soul_value : int = character.get_soul_value_sum()
+	heaven_score += soul_value
+	HeavenScoreLabel.text = "Heaven score is: " + str(heaven_score)
+
+
+func _on_tinder_scene_character_sent_to_hell(character) -> void:
+	var soul_value : int = character.get_soul_value_sum()
+	hell_score += soul_value
+	HellScoreLabel.text = "Hell score is: " + str(hell_score)
+
