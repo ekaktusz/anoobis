f4d107a22ffadccac20ba6b9e13b807da6720681
danigeri <danigerci@gmail.com>
2023-11-25 15:42:33 +0100

    Removed character array, making sure that it is not indexed out-of-bounds.

--- tinder_scene.gd
+++ tinder_scene.gd
@@ -15,13 +15,11 @@ signal character_sent_to_heaven(character: CharacterData)
 
 const PortraitView = preload("res://portrait_view.tscn")
 
-var current_level_10_characters:Array[CharacterData] = []
 var current_character: CharacterData
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	reset_dead_count()
-	current_level_10_characters = CharacterDatabase.get_characters(10)
 	rank_display_label.text = RankDefinitions.get_rank(level)
 	get_new_character()
 
@@ -55,8 +53,9 @@ func swipe_character() -> void:
 
 
 func get_new_character() -> void:
-	self.current_character = current_level_10_characters[processed_dead_count]
+	self.current_character = CharacterDatabase.get_random_character()
 	self.current_character.portrait_view = PortraitView.instantiate()
+
 	character_name.text = current_character.character_name
 	character_changed.emit(self.current_character)
 
