db58081f5edddfc1119271bdc7eccf9566a14866
Csaba Ekart <csaba.ekart@gmail.com>
2023-12-01 18:38:30 +0100

    heaven score

--- game_screens/heaven_scene.gd
+++ game_screens/heaven_scene.gd
@@ -1,10 +1,20 @@
 extends Control
 
 @onready var back_button = $BackButton
+@onready var people = $People
+@onready var heaven_value_label = $HeavenValueLabel
+
+
+# Y-axis values corresponding to the score range [0, 100]
+var minY : float = 600
+var maxY : float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	GlobalGameData.heaven_score = 80
 	BackgroundMusicPlayer.play_heaven_music()
+	heaven_value_label.text = str(GlobalGameData.heaven_score)
+	$People.position.y = lerp(minY, maxY, GlobalGameData.heaven_score / 100.0)
 	pass # Replace with function body.
 
 
