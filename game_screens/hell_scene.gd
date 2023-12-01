76edd1e499e23919d209df30eaf81ea1b1220833
Balazs <balzs.gyngysi@gmail.com>
2023-12-01 16:26:21 +0100

    heaven and hell back buttons

--- game_screens/hell_scene.gd
+++ game_screens/hell_scene.gd
@@ -1,4 +1,5 @@
 extends Control
+@onready var back_button = $BackButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,3 +15,12 @@ var tinder_scene = preload("res://game_screens/tinder_scene.tscn").instantiate()
 func _on_back_button_pressed():
 	SceneTransition.change_scene(tinder_scene)
 	pass # Replace with function body.
+
+
+func _on_back_button_mouse_entered():
+	back_button.scale = Vector2(2.1, 2.1)
+
+
+func _on_back_button_mouse_exited():
+	back_button.scale = Vector2(2, 2)
+
