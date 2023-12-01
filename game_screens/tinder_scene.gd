71e24b5833992028bf50928a0a59bfb02926f5a8
Balazs <balzs.gyngysi@gmail.com>
2023-12-01 17:01:05 +0100

    6/6

--- game_screens/tinder_scene.gd
+++ game_screens/tinder_scene.gd
@@ -39,7 +39,7 @@ func _input(event: InputEvent) -> void:
 
 func swipe_character() -> void:
 	increase_processed_dead_counter()
-	if GlobalGameData.processed_dead_count >= 10:
+	if GlobalGameData.processed_dead_count >= 6:
 		underworld.evaluate_end_of_turn()
 		trigger_break_selector()
 		rank_up()
@@ -57,12 +57,12 @@ func get_new_character() -> void:
 
 func reset_dead_count() -> void:
 	GlobalGameData.processed_dead_count = 0
-	dead_count_label.text = "0/10"
+	dead_count_label.text = "0/6"
 
 
 func increase_processed_dead_counter():
 	GlobalGameData.processed_dead_count += 1
-	dead_count_label.text = str(GlobalGameData.processed_dead_count) + "/10"
+	dead_count_label.text = str(GlobalGameData.processed_dead_count) + "/6"
 
 
 func trigger_break_selector() -> void:
