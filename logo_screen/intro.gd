376daf022451cf965d62278be62b825d92da06ff
danigeri <danigerci@gmail.com>
2024-01-11 12:43:12 +0100

    Intro scene made the main, main menu automatically follows.

--- logo_screen/intro.gd
+++ logo_screen/intro.gd
@@ -5,6 +5,12 @@ func _ready():
 	play()
 	paused = true
 
+
 func _on_timer_timeout() -> void:
 	paused = false
 	$AudioStreamPlayer2D.play()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	var menu_scene : Node = preload("res://game_screens/menu_scene.tscn").instantiate()
+	SceneTransition.change_scene(menu_scene)
